14bb1be43d221aeb9f54137d4dc728a4c9ed5330
SDET-Rudra <rudra4testing@gmail.com>
2025-04-11 17:59:56 +0530

    Assignment_3

---
+++ src/language/basics/Assignment3_Employees.java
@@ -0,0 +1,7 @@
+package language.basics;
+
+public class Assignment3_Employees {
+
+		String[] employeeNames= {"Bharath", "ABC", "XYZ"};
+		int[] employeeIds= {1234, 1235, 1236};
+}
---
+++ src/language/basics/Assignments3_EmpGroups.java
@@ -0,0 +1,29 @@
+package language.basics;
+
+public class Assignments3_EmpGroups extends Assignment3_Employees {
+
+	public static void main(String[] args) {
+		
+		Assignment3_Employees obj=new Assignment3_Employees();
+		
+		String[] employeeNames= new String[3];
+		int[] employeeIds= new int[3];
+		
+		employeeNames[0]=obj.employeeNames[0];
+		employeeNames[1]=obj.employeeNames[1];
+		employeeNames[2]=obj.employeeNames[2];
+		
+		for (int i = 0; i < employeeNames.length; i++) {
+            System.out.println("Employee Name: " + employeeNames[i]);
+        }
+		
+		employeeIds[0]=obj.employeeIds[0];
+		employeeIds[1]=obj.employeeIds[1];
+		employeeIds[2]=obj.employeeIds[2];
+		
+		for (int i = 0; i < employeeIds.length; i++) {
+            System.out.println("Employee ID: " + employeeIds[i]);
+        }	
+	}
+
+}
